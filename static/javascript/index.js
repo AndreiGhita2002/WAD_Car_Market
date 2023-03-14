23213db238602134b5d1c294742a67316c944725
Satveer27 <dabisama27@gmail.com>
2023-03-14 23:29:02 +0000

    finished the main home page and finished footers

---
+++ static/javascript/index.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.show-browse-cars .car-image', { delay:400, origin: 'bottom' });
+ScrollReveal().reveal('.show-browse-cars .text-browse-car', { delay:400, origin: 'right' });
