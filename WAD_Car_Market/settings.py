b20416512896884918737ff1f0a368bf661a2b5e
Satveer27 <dabisama27@gmail.com>
2023-03-17 03:26:34 +0000

    added nice buttons, changed colour and settings

--- WAD_Car_Market/settings.py
+++ WAD_Car_Market/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'crispy_bootstrap4',
     'core',
     'accounts',
     'cars',
@@ -136,5 +134,3 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
