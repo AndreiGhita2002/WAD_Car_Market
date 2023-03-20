5ef1609a800b59f3af2b51af2e58b6dc456fc116
“Mostafa <2724530m@student.gla.ac.uk>
2023-03-20 16:20:13 +0000

    i finished the log in and register completley

--- static/javascript/login.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('user_form');
-    form.classList.add('opacity-0');
-
-    setTimeout(() => {
-        form.classList.remove('opacity-0');
-        form.classList.add('opacity-1');
-    }, 200);
-});
