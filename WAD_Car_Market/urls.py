014fbba30fcbac0af0c747b793a20c40a4d226d0
“Mostafa <2724530m@student.gla.ac.uk>
2023-03-21 10:01:40 +0000

    i added the contact and about page with their css files and i modified the urls to make the links in the nav bar work as intended

--- WAD_Car_Market/urls.py
+++ WAD_Car_Market/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-<<<<<<< HEAD
                   path('admin/', admin.site.urls),
                   path('', views.home, name='home'),
                   path('message_seller/', include('messaging.urls')),
@@ -30,25 +29,5 @@ urlpatterns = [
                   path('about-us/', views.about_us, name='about_us'),
                   path('contact-us/', views.contact_us, name='contact_us'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-        path('admin/', admin.site.urls),
-        path('', views.home, name='home'),
-        path('message_seller/', include('messaging.urls')),
-        path('accounts/', include('accounts.urls')),
-        path('cars/', include('cars.urls')),
-        path('password-reset/',
-                auth_views.PasswordResetView.as_view(
-                template_name='accounts/forgot_password.html'),name='password_reset'),
-        path('password-reset/done/',
-                auth_views.PasswordResetDoneView.as_view(
-                template_name='accounts/password_email.html'),name='password_reset_done'),
-        path('password-reset-confirm/<uidb64>/<token>/',
-                auth_views.PasswordResetConfirmView.as_view(
-                template_name='accounts/change_password.html'),name='password_reset_confirm'),
-        path('password-reset-complete/',
-                auth_views.PasswordResetCompleteView.as_view(
-                template_name='accounts/password_changed.html'),name='password_reset_complete'),
-        ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 1e1c3409b8e4272a2d0e6c8eee89eae922ca6d1b
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
