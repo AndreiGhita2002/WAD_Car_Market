1e1c3409b8e4272a2d0e6c8eee89eae922ca6d1b
Satveer27 <dabisama27@gmail.com>
2023-03-21 03:43:09 +0000

    finish forgot password css/html and backend

--- WAD_Car_Market/urls.py
+++ WAD_Car_Market/urls.py
@@ -15,16 +15,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from core import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', views.home, name='home'),
-                  path('message_seller/', include('messaging.urls')),
-                  path('accounts/', include('accounts.urls')),
-                  path('cars/', include('cars.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        path('admin/', admin.site.urls),
+        path('', views.home, name='home'),
+        path('message_seller/', include('messaging.urls')),
+        path('accounts/', include('accounts.urls')),
+        path('cars/', include('cars.urls')),
+        path('password-reset/',
+                auth_views.PasswordResetView.as_view(
+                template_name='accounts/forgot_password.html'),name='password_reset'),
+        path('password-reset/done/',
+                auth_views.PasswordResetDoneView.as_view(
+                template_name='accounts/password_email.html'),name='password_reset_done'),
+        path('password-reset-confirm/<uidb64>/<token>/',
+                auth_views.PasswordResetConfirmView.as_view(
+                template_name='accounts/change_password.html'),name='password_reset_confirm'),
+        path('password-reset-complete/',
+                auth_views.PasswordResetCompleteView.as_view(
+                template_name='accounts/password_changed.html'),name='password_reset_complete'),
+        ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
