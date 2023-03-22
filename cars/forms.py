6ce860c366f82fdea3b958e7d10eec3291db350a
Ali Ghahramani <2655934g@student.gla.ac.uk>
2023-03-22 01:13:59 +0000

    fixed url pattern

--- cars/forms.py
+++ cars/forms.py
@@ -6,8 +6,8 @@ class CarListingForm(forms.ModelForm):
 
     class Meta:
         model = Car
-        fields = fields = ['title', 'brand', 'model', 'price', 'condition', 'description', 'image',
+        fields = ['title', 'brand', 'model', 'price', 'condition', 'description', 'image',
                            'num_of_seats', 'body_type', 'mileage', 'transmission', 'fuel_type', 'year',
                            'colour', 'location']
 
-        exclude = ['unique_car_id', 'views', 'seller', 'date_posted', ]
+        exclude = ['unique_car_id', 'views', 'seller', 'date_posted']
--- cars/urls.py
+++ cars/urls.py
@@ -6,12 +6,12 @@ app_name = 'cars'
 
 urlpatterns = [
     path('browse/details/<int:car_id>', views.car_details, name='car_details'),
-    path('sell/', views.add_car, name='add_car'),
+    path('sell', views.add_car, name='add_car'),
     # path('saved/', views.saved, name='saved'),  # todo: implement saved cars
 
     # all of these go call views.browse through wrapper functions:
     path('', views.browse_all, name='cars_root'),
     path('<str:args>', views.browse, name='cars_search'),
-    path('used/', views.browse_used, name='cars_used'),
-    path('new/', views.browse_new, name='cars_new'),    
+    path('used', views.browse_used, name='cars_used'),
+    path('new', views.browse_new, name='cars_new'),
 ]
