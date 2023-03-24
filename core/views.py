6361d1fba95cc37e3a631144cf0e9d2960ae7d6e
“Antonis <2578195L@student.gla.ac.uk>
2023-03-24 08:40:46 +0000

    added session cookies

--- core/views.py
+++ core/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 def home(request):
-    return render(request, 'core/index.html')
-
+    visitor_cookie_handler(request)
+    response = render(request, 'core/index.html')
+    return response
 def contact_us(request):
     return render(request, 'core/contact_us.html')
 
 def about_us(request):
     return render(request, 'core/about_us.html')
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+        # Update/set the visits cookie
+    request.session['visits'] = visits
